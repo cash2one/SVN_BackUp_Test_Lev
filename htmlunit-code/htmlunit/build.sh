0f678c9601eecc3d91fbf33c7e95fd3339e4932d
lboyarsky <lboyarsky@76497097-b197-4812-a538-ef37719512e6>
2015-08-21 14:58:59 +0000

    build statistics test

--- htmlunit-code/htmlunit/build.sh
+++ htmlunit-code/htmlunit/build.sh
@@ -1,3 +1,4 @@
 ulimit -n 10000
 chown -R alinebuild /opt/dfinstaller 
+cp aline-devfactory-scripts/tools/build-tools.jar $M2_HOME/lib/ext
 su -l alinebuild -c "java -jar /home/alinebuild/dfbuild.jar --outdir /opt/dfinstaller/dfiout --exec /opt/dfinstaller/src/build_internal.sh"
