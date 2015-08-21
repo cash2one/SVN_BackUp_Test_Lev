f282e496431323fbebc406012b37bc090f77d865
lboyarsky <lboyarsky@76497097-b197-4812-a538-ef37719512e6>
2015-08-21 09:29:22 +0000

    #official

--- htmlunit-code/htmlunit/build.sh
+++ htmlunit-code/htmlunit/build.sh
@@ -1,2 +1,3 @@
+ulimit -n 10000
 chown -R alinebuild /opt/dfinstaller 
 su -l alinebuild -c "java -jar /home/alinebuild/dfbuild.jar --outdir /opt/dfinstaller/dfiout --exec /opt/dfinstaller/src/build_internal.sh"
