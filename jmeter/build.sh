7d73a2d1d2da971b16a251936ba02bc133dc96c9
lboyarsky <lboyarsky@76497097-b197-4812-a538-ef37719512e6>
2015-08-22 16:15:25 +0000

    #official

--- jmeter/build.sh
+++ jmeter/build.sh
@@ -1,4 +1,5 @@
 cd /opt/dfinstaller/src
 ant download_jars
 chown -R alinebuild /opt/dfinstaller
+
 su -l alinebuild -c "java -jar /home/alinebuild/dfbuild.jar --outdir /opt/dfinstaller/dfiout --exec /opt/dfinstaller/src/build_internal.sh"
