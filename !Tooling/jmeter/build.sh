76c007870529006d993f5358ab0a6534df0e7e58
lboyarsky <lboyarsky@76497097-b197-4812-a538-ef37719512e6>
2015-08-23 20:18:56 +0000

    move build scripts to another foolder

--- !Tooling/jmeter/build.sh
+++
@@ -1,5 +0,0 @@
-cd /opt/dfinstaller/src
-ant download_jars
-chown -R alinebuild /opt/dfinstaller
-
-su -l alinebuild -c "java -jar /home/alinebuild/dfbuild.jar --outdir /opt/dfinstaller/dfiout --exec /opt/dfinstaller/src/build_internal.sh"
--- !Tooling/jmeter/build_test.sh
+++
@@ -1,2 +0,0 @@
-cd /opt/dfinstaller/src
-ant $DF_ANT_BUILD_STATS_OPTS test
